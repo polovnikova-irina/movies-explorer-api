45dbd91a1ece7b11f7df844c9f782507bb235896
unknown <polovnikovai19i@gmail.com>
2023-10-16 16:55:44 +0300

    изменила config 4

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 
+const DB_URL = process.env.DB_URL || 'mongodb://127.0.0.1:27017/bitfilmsdb';
+const PORT = process.env.PORT || 3000;
+
 module.exports = {
-  DB_URL: process.env.DB_URL || 'mongodb://127.0.0.1:27017/bitfilmsdb',
-  port: process.env.PORT || 3000,
+  DB_URL,
+  PORT,
 };
