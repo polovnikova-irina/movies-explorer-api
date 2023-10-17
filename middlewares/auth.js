d51a76054c84c3fd07a0757deb97cde096ab8c38
unknown <polovnikovai19i@gmail.com>
2023-10-17 11:35:40 +0300

    исправление conflict error

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../utils/errors/UnauthorizedError');
+const { UserErrorMessage } = require('../utils/constants');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
@@ -7,7 +8,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new UnauthorizedError('Неправильные почта или пароль'));
+    return next(new UnauthorizedError(UserErrorMessage.Unauthorized));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -16,7 +17,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return next(new UnauthorizedError('Неправильные почта или пароль'));
+    return next(new UnauthorizedError(UserErrorMessage.Unauthorized));
   }
 
   req.user = payload;
